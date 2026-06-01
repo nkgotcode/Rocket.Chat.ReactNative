fec0229eef8839b1a5d67230233ea9c467fa2b0b
yash-rajpal <rajpal.yash03@gmail.com>
2026-06-02 00:30:10 +0530

    fix tests and snapshots

--- app/containers/LoginServices/LoginServices.stories.tsx
+++ app/containers/LoginServices/LoginServices.stories.tsx
@@ -67,8 +67,8 @@ export default {
 
 export const Separators = () => (
 	<>
-		<ServicesSeparator collapsed onPress={() => {}} separator services={services} />
-		<ServicesSeparator collapsed={false} onPress={() => {}} separator services={services} />
+		<ServicesSeparator collapsed onPress={() => {}} separator totalServices={4} />
+		<ServicesSeparator collapsed={false} onPress={() => {}} separator totalServices={4} />
 	</>
 );
 
