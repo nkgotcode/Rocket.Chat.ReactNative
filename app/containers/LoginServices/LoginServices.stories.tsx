b9ca933c46e30558c114969fcb01e3714c24d77d
yash-rajpal <rajpal.yash03@gmail.com>
2026-06-01 21:24:29 +0530

    fix review and stories

--- app/containers/LoginServices/LoginServices.stories.tsx
+++ app/containers/LoginServices/LoginServices.stories.tsx
@@ -8,6 +8,7 @@ import ServicesSeparator from './ServicesSeparator';
 import ButtonService from './ButtonService';
 import { type IServices } from '../../selectors/login';
 import { type TIconsName } from '../CustomIcon';
+import ServiceListTest from './ServiceList';
 
 const styles = StyleSheet.create({
 	serviceName: {
@@ -94,3 +95,31 @@ export const ServiceList = () => (
 		})}
 	</>
 );
+
+export const ServciceListCollapsed = () => {
+	return (
+		<ServiceListTest
+			services={services}
+			CAS_enabled={false}
+			CAS_login_url=''
+			Gitlab_URL=''
+			server='https://demo.rocket.chat'
+			collapsed={true}
+			showLoginOnWebButton={true}
+		/>
+	);
+};
+
+export const ServciceListUncollapsed = () => {
+	return (
+		<ServiceListTest
+			services={services}
+			CAS_enabled={false}
+			CAS_login_url=''
+			Gitlab_URL=''
+			server='https://demo.rocket.chat'
+			collapsed={false}
+			showLoginOnWebButton={true}
+		/>
+	);
+};
